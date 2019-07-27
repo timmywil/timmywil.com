93f97ae4bb0419b8b03ff2408e11fc402c900bfc
Timmy Willison <4timmywil@gmail.com>
2019-07-26 23:57:41 -0400

    fix: add jquery.panzoom redirects to panzoom

---
+++ src/pages/jquery.panzoom.tsx
@@ -0,0 +1,8 @@
+import React, { useEffect } from 'react'
+
+export default function PanzoomRedirect() {
+  useEffect(() => {
+    window.location.href = 'https://timmywil.com/panzoom/'
+  }, [])
+  return <div />
+}
